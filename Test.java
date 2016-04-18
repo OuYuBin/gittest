c18df4bacb38ac19ab93509598d460ccda969c4d
Robin Ou <82324767@qq.com>
2016-04-18 10:12:50 +0800

    first commit

---
+++ Test.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.cpic.caf.component.ui.provider;
+
+/**
+ * @author robin
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
